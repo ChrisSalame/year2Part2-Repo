9e31dfde0961be322511d226f327efdce0401bd5
ChrisSalame <148483955+ChrisSalame@users.noreply.github.com>
2025-03-17 16:55:21 -0400

    addedInClass

--- year2Part2/Assets/Scipts/changeColorAT.cs
+++ year2Part2/Assets/Scipts/changeColorAT.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 namespace NodeCanvas.Tasks.Actions {
 
-	public class moveToTargetAT : ActionTask {
+	public class changeColorAT : ActionTask {
 
-		public BBParameter<Transform> currentTarget;
-		public float speed;
+		public MeshRenderer meshRenderer;
+		public Color colorToChangeTo;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -20,9 +20,8 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			Vector3 directionToMove = (currentTarget.value.position - agent.transform.position);
-			agent.transform.position += directionToMove.normalized * speed * Time.deltaTime;
-			EndAction(true);
+			meshRenderer.material.color = colorToChangeTo;
+			//EndAction(true);
 		}
 
 		//Called once per frame while the action is active.
